770e4d62af272a729b43b6b838075249672daeae
Luis Slocker <slockerboss@gmail.com>
2012-04-12 16:28:12 +0200

    version 1 working

--- gen/com/pruebas/slocker/R.java
+++ gen/com/pruebas/slocker/R.java
@@ -11,45 +11,65 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int Azul=0x7f040001;
-        public static final int Rojo=0x7f040000;
-        public static final int Verde=0x7f040002;
+        public static final int Azul=0x7f040003;
+        public static final int Blanco=0x7f040001;
+        public static final int Negro=0x7f040000;
+        public static final int Rojo=0x7f040002;
+        public static final int Verde=0x7f040004;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_launcher_yuyi=0x7f020001;
+        public static final int fondo1=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_launcher_yuyi=0x7f020002;
     }
     public static final class id {
-        public static final int LayautSeleccionaHazOp=0x7f060009;
-        public static final int LayoutCabecera=0x7f060000;
-        public static final int LayoutCuerpo=0x7f060001;
-        public static final int LayoutOperando1=0x7f060003;
-        public static final int LayoutOperando2=0x7f060006;
-        public static final int LayoutOperandos=0x7f060002;
-        public static final int LayoutPie=0x7f060013;
-        public static final int botonBorra=0x7f060011;
-        public static final int botonOperacion=0x7f06000e;
-        public static final int botonSuma=0x7f060012;
-        public static final int operando1=0x7f060005;
-        public static final int operando2=0x7f060008;
-        public static final int radio0=0x7f06000b;
-        public static final int radio1=0x7f06000c;
-        public static final int radio2=0x7f06000d;
-        public static final int radioGrupoOperacion=0x7f06000a;
-        public static final int resultado=0x7f06000f;
-        public static final int textView1=0x7f060004;
-        public static final int textView2=0x7f060007;
-        public static final int textViewInfo=0x7f060010;
+        public static final int ErrorResultado=0x7f070013;
+        public static final int LayaoutBotonOPYresultado=0x7f070010;
+        public static final int LayaoutRadiobuttons=0x7f07000a;
+        public static final int LayoutBorraOperandos=0x7f070015;
+        public static final int LayoutCabecera=0x7f070000;
+        public static final int LayoutContenido=0x7f070009;
+        public static final int LayoutCuerpo=0x7f070001;
+        public static final int LayoutOperando1=0x7f070003;
+        public static final int LayoutOperando2=0x7f070006;
+        public static final int LayoutOperandos=0x7f070002;
+        public static final int LayoutPie=0x7f070018;
+        public static final int botonBorra=0x7f070017;
+        public static final int botonOperacion=0x7f070011;
+        public static final int button1=0x7f07001a;
+        public static final int editText1=0x7f070019;
+        public static final int operando1=0x7f070005;
+        public static final int operando2=0x7f070008;
+        public static final int radio0=0x7f07000c;
+        public static final int radio1=0x7f07000d;
+        public static final int radio2=0x7f07000e;
+        public static final int radio3=0x7f07000f;
+        public static final int radioGrupoOperacion=0x7f07000b;
+        public static final int resultado=0x7f070014;
+        public static final int textView1=0x7f070004;
+        public static final int textView2=0x7f070007;
+        public static final int textViewInfo=0x7f070016;
+        public static final int tituloResultado=0x7f070012;
     }
     public static final class layout {
         public static final int calculadora=0x7f030000;
-        public static final int calculadora_new=0x7f030001;
-        public static final int main=0x7f030002;
+        public static final int main=0x7f030001;
     }
     public static final class string {
+        public static final int BotonRealizarOperacion=0x7f050008;
+        public static final int Dividir=0x7f050006;
+        public static final int Multiplicar=0x7f050005;
+        public static final int Restar=0x7f050004;
+        public static final int Sumar=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int botonBorrarOperandos=0x7f05000b;
+        public static final int operando_2Hint=0x7f050007;
         public static final int primerOp=0x7f050001;
-        public static final int resultado=0x7f050003;
         public static final int segundoOp=0x7f050002;
+        public static final int textViewInfo_texto=0x7f05000a;
+        public static final int tituloResultado=0x7f050009;
+    }
+    public static final class style {
+        public static final int styleName=0x7f060000;
     }
 }
--- src/com/pruebas/slocker/SumaNumActivity.java
+++ src/com/pruebas/slocker/SumaNumActivity.java
@@ -1,21 +1,25 @@
 package com.pruebas.slocker;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class SumaNumActivity extends Activity {
 
-	private TextView textView1, textView2, textViewResult, textViewInfo;
+	private TextView textViewResult, textViewInfo, textViewErrorResult;
 	private EditText editTextOp1, editTextOp2;
-	private Button botonSuma, botonBorrar, botonOperacion;
-	private RadioButton radioButton1;
+	private Button botonBorrar, botonOperacion;
+	private LinearLayout layoutContenido;
 
 	protected String stringOp1, stringOp2;
 	float op1, op2, resultado;
@@ -28,48 +32,16 @@ public class SumaNumActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.calculadora);
 
-		textView1 = (TextView) findViewById(R.id.textView1);
-		textView2 = (TextView) findViewById(R.id.textView2);
 		textViewResult = (TextView) findViewById(R.id.resultado);
+		textViewErrorResult = (TextView) findViewById(R.id.ErrorResultado);
 		textViewInfo = (TextView) findViewById(R.id.textViewInfo);
 
 		editTextOp1 = (EditText) findViewById(R.id.operando1);
 		editTextOp2 = (EditText) findViewById(R.id.operando2);
 
-		botonSuma = (Button) findViewById(R.id.botonSuma);
-		botonSuma.setOnClickListener(new OnClickListener() {
+		layoutContenido = (LinearLayout) findViewById(R.id.LayoutContenido);
+//		layoutContenido.setBackgroundColor(Color.parseColor("#55ffffff"));
 
-			@Override
-			public void onClick(View arg0) {
-				op1Empty = false;
-				op2Empty = false;
-
-				textViewResult.setText("");
-
-				stringOp1 = editTextOp1.getText().toString();
-				try {
-					op1 = Float.parseFloat(stringOp1);
-
-				} catch (Exception e) {
-					op1Empty = true;
-					textViewResult.append("Operando 1 invalido\n");
-				}
-
-				stringOp2 = editTextOp2.getText().toString();
-				try {
-					op2 = Float.parseFloat(stringOp2);
-				} catch (Exception e) {
-					op2Empty = true;
-					textViewResult.append("Operando 2 invalido\n");
-				}
-
-				resultado = suma(op1, op2);
-				if (!op1Empty && !op2Empty) {
-					textViewResult.setText("El resultado es: " + resultado);
-				}
-
-			}
-		});
 
 		botonBorrar = (Button) findViewById(R.id.botonBorra);
 		botonBorrar.setOnClickListener(new OnClickListener() {
@@ -81,7 +53,6 @@ public class SumaNumActivity extends Activity {
 			}
 		});
 
-		radioButton1 = (RadioButton) findViewById(R.id.radio0);
 		radioGrupoOperacion = (RadioGroup) findViewById(R.id.radioGrupoOperacion);
 
 		botonOperacion = (Button) findViewById(R.id.botonOperacion);
@@ -89,74 +60,95 @@ public class SumaNumActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
+				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(editTextOp1.getWindowToken(), 0);
+
+				op1Empty = false;
+				op2Empty = false;
+				textViewErrorResult.setText("");
 
 				tipoOperacion = radioGrupoOperacion.getCheckedRadioButtonId();
 				switch (tipoOperacion) {
 				case (R.id.radio0):
 					textViewInfo.setText("ESTAS SUMANDO");
-					op1Empty = false;
-					op2Empty = false;
 
-					textViewResult.setText("");
-
-					stringOp1 = editTextOp1.getText().toString();
-					try {
-						op1 = Float.parseFloat(stringOp1);
-
-					} catch (Exception e) {
-						op1Empty = true;
-						textViewResult.append("Operando 1 invalido\n");
-					}
-
-					stringOp2 = editTextOp2.getText().toString();
-					try {
-						op2 = Float.parseFloat(stringOp2);
-					} catch (Exception e) {
-						op2Empty = true;
-						textViewResult.append("Operando 2 invalido\n");
-					}
-
+					obtenerOp1();
+					obtenerOp2();
 					resultado = suma(op1, op2);
-					if (!op1Empty && !op2Empty) {
-						textViewResult.setText("El resultado es: " + resultado);
-					}
+					mostrarResultado();
 					break;
+
 				case (R.id.radio1):
 					textViewInfo.setText("ESTAS RESTANDO");
-					op1Empty = false;
-					op2Empty = false;
 
-					textViewResult.setText("");
-
-					stringOp1 = editTextOp1.getText().toString();
-					try {
-						op1 = Float.parseFloat(stringOp1);
-
-					} catch (Exception e) {
-						op1Empty = true;
-						textViewResult.append("Operando 1 invalido\n");
-					}
-
-					stringOp2 = editTextOp2.getText().toString();
-					try {
-						op2 = Float.parseFloat(stringOp2);
-					} catch (Exception e) {
-						op2Empty = true;
-						textViewResult.append("Operando 2 invalido\n");
-					}
+					obtenerOp1();
+					obtenerOp2();
 
 					resultado = resta(op1, op2);
-					if (!op1Empty && !op2Empty) {
-						textViewResult.setText("El resultado es: " + resultado);
-					}
+					mostrarResultado();
 					break;
 				case (R.id.radio2):
-					textViewInfo.setText("radio 2");
+					textViewInfo.setText("ESTAS MULTIPLICANDO");
+
+					obtenerOp1();
+					obtenerOp2();
+
+					resultado = multiplica(op1, op2);
+					mostrarResultado();
+					break;
+				case (R.id.radio3):
+					textViewInfo.setText("ESTAS DIVIDIENDO");
+
+					obtenerOp1();
+					obtenerOp2();
+					resultado = divide(op1, op2);
+					mostrarResultado();
+					break;
+				default:
+					textViewInfo.setText("error con lso radiobuttons");
 					break;
 				}
 
 			}
 		});
+	}
+
+	protected void obtenerOp1() {
+		stringOp1 = editTextOp1.getText().toString();
+		try {
+			op1 = Float.parseFloat(stringOp1);
+
+		} catch (Exception e) {
+			op1Empty = true;
+			textViewErrorResult.append("Operando 1 invalido\n");
+		}
+	}
+
+	protected void obtenerOp2() {
+		stringOp2 = editTextOp2.getText().toString();
+		try {
+			op2 = Float.parseFloat(stringOp2);
+		} catch (Exception e) {
+			op2Empty = true;
+			textViewErrorResult.append("Operando 2 invalido\n");
+		}
+
+	}
+
+	protected void mostrarResultado() {
+		if (!op1Empty && !op2Empty) {
+			textViewResult.setText("" + resultado);
+		}
+
+	}
+
+	protected void borrarOperandos() {
+		editTextOp1.setText("");
+		editTextOp2.setText("");
+		textViewResult.setText("");
+		textViewErrorResult.setText("");
+		textViewInfo.setText("");
+
 	}
 
 	protected float suma(float op1, float op2) {
@@ -167,11 +159,11 @@ public class SumaNumActivity extends Activity {
 		return op1 - op2;
 	}
 
-	protected void borrarOperandos() {
-		editTextOp1.setText("");
-		editTextOp2.setText("");
-		textViewResult.setText("");
-		textViewInfo.setText("");
+	protected float multiplica(float op1, float op2) {
+		return op1 * op2;
+	}
 
+	protected float divide(float op1, float op2) {
+		return op1 / op2;
 	}
 }
